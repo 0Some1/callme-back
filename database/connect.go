537c8fbce944f731edb5c12e4adadf9fec48d628
m.sadegh <mghaleyani@gmail.com>
2021-10-16 08:31:52 -0400

    .env file added and DB is dynamic through .env

--- database/connect.go
+++ database/connect.go
@@ -1,21 +1,32 @@
 package database
 
 import (
+	"callme/config"
 	"callme/models"
+	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
-func Connect()  {
-	database , err:= gorm.Open(mysql.Open("root:4935@/test0"),&gorm.Config{})
-	if err!=nil {
+
+func Connect() {
+
+	dns := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+		config.Config("DB_USER"),
+		config.Config("DB_PASSWORD"),
+		config.Config("DB_HOST"),
+		config.Config("DB_PORT"),
+		config.Config("DB_NAME"))
+
+	database, err := gorm.Open(mysql.Open(dns), &gorm.Config{})
+	if err != nil {
 		panic("could not connect to database")
 	}
-	DB=database
+	DB = database
 	migration(DB)
 }
 
-func migration(db *gorm.DB )  {
+func migration(db *gorm.DB) {
 	db.AutoMigrate(&models.User{})
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/go-playground/validator/v10 v10.9.0 // indirect
 	github.com/gofiber/fiber/v2 v2.20.2
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/valyala/fasthttp v1.31.0 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
