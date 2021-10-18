0def0812230052d0ef89ba71d3ea2362005d33e8
m.sadegh <mghaleyani@gmail.com>
2021-10-18 13:58:13 -0400

    fix a reported bug about "duplicate email"

--- models/user.go
+++ models/user.go
@@ -5,6 +5,6 @@ type User struct {
 	Username  string `validate:"required,min=3,max=32" json:"username"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
-	Email     string `validate:"required,email,max=32" gorm:"unique" json:"email"`
+	Email     string `gorm:"unique" validate:"required,email,max=32" json:"email"`
 	Password  []byte `validate:"required" json:"-"`
 }
