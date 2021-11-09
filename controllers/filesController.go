fd88de960779736e0a5e3c96faa5e24e8b17ba34
m.sadegh <mghaleyani@gmail.com>
2021-11-09 19:58:05 +0330

    het image of post is somehow completed.

--- controllers/filesController.go
+++ controllers/filesController.go
@@ -1,6 +1,10 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"callme/database"
+	"callme/models"
+	"github.com/gofiber/fiber/v2"
+)
 
 func GetProfileImage(c *fiber.Ctx) error {
 	err := c.SendFile("./uploads/profile/" + c.Params("filename"))
@@ -11,9 +15,34 @@ func GetProfileImage(c *fiber.Ctx) error {
 }
 
 func GetPostImage(c *fiber.Ctx) error {
-	err := c.SendFile("./uploads/posts/" + c.Params("filename"))
+	user := c.Locals("user").(*models.User)
+	post := new(models.Post)
+	filename := c.Params("filename")
+	post, err := database.DB.GetPostByPhotoName(filename)
 	if err != nil {
-		return fiber.ErrNotFound
+		return fiber.NewError(fiber.StatusNotFound, "Post not found")
 	}
-	return nil
+	if post.UserID == user.ID {
+		err = c.SendFile("./uploads/post/" + filename)
+		if err != nil {
+			return fiber.ErrNotFound
+		}
+		return nil
+	}
+
+	err = database.DB.PreloadFollowings(user)
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, "Internal server error")
+	}
+	//it is not tested , and it might have issues
+	if user.IsFollowing(post.UserID) {
+		err = c.SendFile("./uploads/post/" + filename)
+		if err != nil {
+			return fiber.ErrNotFound
+		}
+		return nil
+	}
+
+	return fiber.NewError(fiber.StatusForbidden, "You are not allowed to see this post")
+
 }
