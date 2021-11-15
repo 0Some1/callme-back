fe83c416db457a64f3110e629fc57099fb290506
m.sadegh <mghaleyani@gmail.com>
2021-11-15 14:02:53 +0330

    cleaning

--- routes/routes.go
+++ routes/routes.go
@@ -7,18 +7,19 @@ import (
 )
 
 func Setup(api fiber.Router) {
+	//authentication routes
 	api.Post("/register", controllers.Register)
 	api.Post("/login", controllers.Login)
 	api.Use(middlewares.IsAuthenticated)
 
+	//profile or user routes
 	api.Get("/profile", controllers.GetUser)
 	api.Patch("/Profile", controllers.UpdateUser)
 	api.Delete("/Profile", controllers.DeleteUser)
-
 	api.Put("/avatar", controllers.UpdateAvatar)
 
+	//post routes
 	api.Get("/posts", controllers.GetPosts)
-
 	api.Post("/post", controllers.CreatePost)
 
 }
