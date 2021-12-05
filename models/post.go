a092168af7633dd204b328baed5b73b979080db7
m.sadegh <mghaleyani@gmail.com>
2021-12-05 20:25:37 +0330

    [fix] user and comment should pass by ref

--- models/post.go
+++ models/post.go
@@ -5,13 +5,13 @@ import "gorm.io/gorm"
 type Post struct {
 	gorm.Model
 	UserID      uint
-	Private     *bool     `json:"private,omitempty" gorm:"type:bool;default:false"`
-	Title       string    `json:"title,omitempty" validate:"required"`
-	Photos      []*Photo  `json:"photos,omitempty" gorm:"foreignKey:PostID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;preload:true"`
-	Description string    `json:"description,omitempty" validate:"required"`
-	Keywords    string    `json:"keywords,omitempty" validate:"required"`
-	Likes       []User    `json:"likes,omitempty" gorm:"many2many:user_like;"`
-	Comments    []Comment `json:"comments,omitempty"`
+	Private     *bool      `json:"private,omitempty" gorm:"type:bool;default:false"`
+	Title       string     `json:"title,omitempty" validate:"required"`
+	Photos      []*Photo   `json:"photos,omitempty" gorm:"foreignKey:PostID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;preload:true"`
+	Description string     `json:"description,omitempty" validate:"required"`
+	Keywords    string     `json:"keywords,omitempty" validate:"required"`
+	Likes       []*User    `json:"likes,omitempty" gorm:"many2many:user_like;"`
+	Comments    []*Comment `json:"comments,omitempty"`
 }
 
 func (p *Post) PreparePost(baseURL string) {
