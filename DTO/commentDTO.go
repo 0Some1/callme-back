216444b3dcb4264c9bddf3434b7470c26d80524f
Arash <arash1.ramzani@gmail.com>
2021-12-17 00:58:48 +0330

    [fix] added fields for user details

--- DTO/commentDTO.go
+++ DTO/commentDTO.go
@@ -1,12 +1,17 @@
 package DTO
 
 import (
+	"callme/database"
 	"callme/models"
+	"strconv"
 )
 
 type CommentDTO struct {
 	ID         uint
 	UserID     uint
+	UserName   string
+	Avatar     string
+	Bio        string
 	PostID     uint
 	Text       string
 	OwnComment bool //if the requesting user has posted this comment
@@ -15,10 +20,18 @@ type CommentDTO struct {
 func PrepareCommentDTO(userID uint, comments []*models.Comment) []*CommentDTO {
 	commentDTOs := make([]*CommentDTO, 0)
 	for i := range comments {
+		userIDString := strconv.FormatUint(uint64(comments[i].UserID), 10)
+		user, err := database.DB.GetUserByID(userIDString)
+		if err != nil {
+			continue
+		}
 		commentDTOs = append(commentDTOs, &CommentDTO{
 			ID:         comments[i].ID,
 			UserID:     comments[i].UserID,
 			PostID:     comments[i].PostID,
+			UserName:   user.Username,
+			Avatar:     user.Avatar,
+			Bio:        user.Bio,
 			Text:       comments[i].Text,
 			OwnComment: comments[i].UserID == userID,
 		})
