79d8f91efa0b9944b4dd05d09ea55b9f3d20520a
Arash <arash1.ramzani@gmail.com>
2021-12-20 23:12:58 +0330

    [add] PrepareCommentDTO creates a dto from comment

--- DTO/commentDTO.go
+++ DTO/commentDTO.go
@@ -17,7 +17,7 @@ type CommentDTO struct {
 	OwnComment bool //if the requesting user has posted this comment
 }
 
-func PrepareCommentDTO(userID uint, comments []*models.Comment) []*CommentDTO {
+func PrepareCommentDTOs(userID uint, comments []*models.Comment) []*CommentDTO {
 	commentDTOs := make([]*CommentDTO, 0)
 	for i := range comments {
 		userIDString := strconv.FormatUint(uint64(comments[i].UserID), 10)
@@ -35,6 +35,25 @@ func PrepareCommentDTO(userID uint, comments []*models.Comment) []*CommentDTO {
 			Text:       comments[i].Text,
 			OwnComment: comments[i].UserID == userID,
 		})
+	}
+	return commentDTOs
+}
+
+func PrepareCommentDTO(userID uint, comment *models.Comment) CommentDTO {
+	userIDString := strconv.FormatUint(uint64(comment.UserID), 10)
+	user, err := database.DB.GetUserByID(userIDString)
+	if err != nil {
+		return CommentDTO{}
+	}
+	commentDTOs := CommentDTO{
+		ID:         comment.ID,
+		UserID:     comment.UserID,
+		PostID:     comment.PostID,
+		UserName:   user.Username,
+		Avatar:     user.Avatar,
+		Bio:        user.Bio,
+		Text:       comment.Text,
+		OwnComment: comment.UserID == userID,
 	}
 	return commentDTOs
 }
